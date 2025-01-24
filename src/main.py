75a7367ea78668a7f6e6011679510e2212aa0dd1
Karol <karol.sykala@protonmail.com>
2025-01-24 13:53:44 +0100

    add textnode functions

--- src/main.py
+++ src/main.py
@@ -1,1 +1,6 @@
-print('Hello Bitch')
+from textnode import TextNode
+def main():
+    newNode = TextNode("Text node", "bold", "www.google.com")
+    print(newNode)
+
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -7,7 +7,15 @@ class Bender(Enum):
     FIRE_BENDER = "fire"
 
 class TextNode:
-    def __init(self, text, text_type, url):
+    def __init__(self, text, text_type, url):
         self.text = text
         self.text_type = text_type
         self.url = url
+    
+    def __eq__(self, object):
+        print(self.__dict__)
+        print(object.__dict__)
+        return self.__dict__ == object.__dict__
+    
+    def __repr__(self):
+        return f'TextNode({self.text}, {self.text_type}, {self.url})'
