ae8758dea061b0ef96ea6be83e5c1851cbc9e915
Karol Sykała <karol.sykala@protonmail.com>
2025-02-02 18:48:39 +0100

    add leafnode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -14,7 +14,23 @@ class HTMLNode:
     def __repr__(self):
         return f'{self.tag}\n{self.value}\n{self.children}\n{self.props}'
 
-seks = HTMLNode('uwu', 'nya', 'arf', 'sram')
+seks = HTMLNode('uwu', 'nya', 'arf', {"sex": "yes"})
 
 
-#print(seks.__repr__())
+print(seks.__repr__())
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value):
+        super().__init__(tag, value, children=None)
+    
+    def to_html(self):
+        if not self.value:
+            raise ValueError
+        elif self.tag == None or not self.tag:
+            return f'{self.value}'
+        else:
+            if self.props:
+                props_str = " ".join([f'{key}="{value}"' for key, value in self.props.items()])
+                return f'<{self.tag} {props_str}>{self.value}</{self.tag}>'
+            else:
+                return f'<{self.tag}>{self.value}</{self.tag}>'
