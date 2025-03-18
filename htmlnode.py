5f53990a40da3cc9116bc63bcdbbbba136363b17
mlem_dev <karol.sykala@protonmail.com>
2025-03-18 20:42:55 +0100

    add basic tests

---
+++ htmlnode.py
@@ -0,0 +1,48 @@
+   static_site_generator
+ ├╴  public
+ ├╴  src
+ │ ├╴  main.py
+ │ ├╴  test_textnode.py
+ │ └╴  textnode.py
+ ├╴  main.sh
+ └╴  test.sh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,14 +1,23 @@
-   static_site_generator
- ├╴  public
- ├╴  src
- │ ├╴  main.py
- │ └╴  textnode.py
- └╴  main.sh
+import unittest
 
+from textnode import TextNode, TextType
 
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        node3 = TextNode("This is another text node", TextType.CODE)
+        node4 = TextNode("This is another text node", TextType.CODE)
+        self.assertEqual(node, node2)
+        self.assertEqual(node3, node4)
 
+    def test_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is another text node", TextType.CODE)
+        self.assertNotEqual(node, node2)
 
-
+if __name__ == "__main__":
+    unittest.main()
 
 
 
